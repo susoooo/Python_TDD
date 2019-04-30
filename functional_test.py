a3e3dd1ab9c3702f044df5d4cd348d43da68275a
Jesús Otero Seoane <susoo@MacBook-Pro-de-Jesus-Otero-Seoane.local>
2019-04-30 12:30:13 +0200

    Basic view now returns minimal HTLM

--- functional_test.py
+++ functional_test.py
@@ -19,7 +19,7 @@ class NewVisitorTest(unittest.TestCase):
 		self.browser.get('http://localhost:8000')
 
 		#She notices the page title and header mention to-do lists
-		self.assertIn('to-do', self.browser.title) 
+		self.assertIn('To-Do', self.browser.title) 
 		self.fail('Finish the test!')
 
 		#She is invited to enter a to-do item stright away
